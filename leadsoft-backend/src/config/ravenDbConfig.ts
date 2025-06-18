747352cbe1828e1d87b65270a833bd4df7b53fee
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-06-18 18:47:34 -0300

    ajustes deploy

--- leadsoft-backend/src/config/ravenDbConfig.ts
+++ leadsoft-backend/src/config/ravenDbConfig.ts
@@ -3,22 +3,35 @@ import { DocumentStore } from 'ravendb';
 import path from 'path';
 import fs from 'fs';
 import tls from 'tls';
+
 dotenv.config();
 
-tls.checkServerIdentity = () => undefined; // ⚠️ ignora hostname do SSL
+tls.checkServerIdentity = () => undefined;
+
+if (process.env.NODE_ENV !== 'production') {
+  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+}
 process.env['RAVEN_NodeJs_UseIpv6'] = 'false';
 
 export const getRavenDbConnection = (): DocumentStore => {
-  const certificatePath = path.resolve(__dirname, 'backend.pfx');
+  const certificatePath = process.env.RAVEN_CERT_PATH
+    ? path.resolve(process.env.RAVEN_CERT_PATH)
+    : path.resolve(__dirname, 'backend.pfx');
+
   const certificateBuffer = fs.readFileSync(certificatePath);
   console.log("📁 Carregando certificado de:", certificatePath);
   console.log("🔐 Tamanho do buffer do certificado:", certificateBuffer.length);
   console.log("🔍 RAVEN_URL:", process.env.RAVEN_URL);
-  const store = new DocumentStore(process.env.RAVEN_URL!, process.env.RAVEN_DATABASE!, {
-    type: 'pfx',
-    certificate: certificateBuffer,
-    password: process.env.RAVEN_CERT_PASSWORD || '',
-  });
+
+  const store = new DocumentStore(
+    process.env.RAVEN_URL!,
+    process.env.RAVEN_DATABASE!,
+    {
+      type: 'pfx',
+      certificate: certificateBuffer,
+      password: process.env.RAVEN_CERT_PASSWORD || '',
+    }
+  );
 
   store.initialize();
   return store;

a7f4c3cfc5c25b4f9747f19ee298cce45b1ef75b
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-06-18 18:43:25 -0300

    ajustes deploy

--- leadsoft-backend/src/config/ravenDbConfig.ts
+++ leadsoft-backend/src/config/ravenDbConfig.ts
@@ -6,13 +6,12 @@ import tls from 'tls';
 dotenv.config();
 
 tls.checkServerIdentity = () => undefined; // ⚠️ ignora hostname do SSL
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 process.env['RAVEN_NodeJs_UseIpv6'] = 'false';
 
 export const getRavenDbConnection = (): DocumentStore => {
   const certificatePath = path.resolve(__dirname, 'backend.pfx');
-  console.log("📁 Carregando certificado de:", certificatePath);
   const certificateBuffer = fs.readFileSync(certificatePath);
+  console.log("📁 Carregando certificado de:", certificatePath);
   console.log("🔐 Tamanho do buffer do certificado:", certificateBuffer.length);
   console.log("🔍 RAVEN_URL:", process.env.RAVEN_URL);
   const store = new DocumentStore(process.env.RAVEN_URL!, process.env.RAVEN_DATABASE!, {

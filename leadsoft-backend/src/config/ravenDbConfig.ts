ece68bf7c39be848b9d9838fedfee82035488597
Mariah Hoffmann <mariah.hoffmann@gmail.com>
2025-06-19 12:28:12 -0300

    ajustes deploy

--- leadsoft-backend/src/config/ravenDbConfig.ts
+++ leadsoft-backend/src/config/ravenDbConfig.ts
@@ -13,23 +13,50 @@ if (process.env.NODE_ENV !== 'production') {
 }
 process.env['RAVEN_NodeJs_UseIpv6'] = 'false';
 
+// export const getRavenDbConnection = (): DocumentStore => {
+//   const certificatePath = process.env.RAVEN_CERT_PATH || path.resolve(__dirname, 'backend.pfx');
+
+//   const certificateBuffer = fs.readFileSync(certificatePath);
+//   console.log("📁 Carregando certificado de:", certificatePath);
+//   console.log("🔐 Tamanho do buffer do certificado:", certificateBuffer.length);
+//   console.log("🔍 RAVEN_URL:", process.env.RAVEN_URL);
+
+//   const store = new DocumentStore(
+//     process.env.RAVEN_URL!,
+//     process.env.RAVEN_DATABASE!,
+//     {
+//       type: 'pfx',
+//       certificate: certificateBuffer,
+//       password: process.env.RAVEN_CERT_PASSWORD || '',
+//     }
+//   );
+
+//   store.initialize();
+//   return store;
+// };
 export const getRavenDbConnection = (): DocumentStore => {
-  const certificatePath = process.env.RAVEN_CERT_PATH || path.resolve(__dirname, 'backend.pfx');
+  const ravenUrl = process.env.RAVEN_URL!;
+  const ravenDb = process.env.RAVEN_DATABASE!;
 
-  const certificateBuffer = fs.readFileSync(certificatePath);
-  console.log("📁 Carregando certificado de:", certificatePath);
-  console.log("🔐 Tamanho do buffer do certificado:", certificateBuffer.length);
-  console.log("🔍 RAVEN_URL:", process.env.RAVEN_URL);
+  let store: DocumentStore;
 
-  const store = new DocumentStore(
-    process.env.RAVEN_URL!,
-    process.env.RAVEN_DATABASE!,
-    {
+  if (ravenUrl.startsWith('https')) {
+    const certificatePath = process.env.RAVEN_CERT_PATH || path.resolve(__dirname, 'backend.pfx');
+    const certificateBuffer = fs.readFileSync(certificatePath);
+
+    console.log("📁 Carregando certificado de:", certificatePath);
+    console.log("🔐 Tamanho do buffer do certificado:", certificateBuffer.length);
+    console.log("🔍 RAVEN_URL:", ravenUrl);
+
+    store = new DocumentStore(ravenUrl, ravenDb, {
       type: 'pfx',
       certificate: certificateBuffer,
       password: process.env.RAVEN_CERT_PASSWORD || '',
-    }
-  );
+    });
+  } else {
+    console.log("⚠️ Conectando sem certificado");
+    store = new DocumentStore(ravenUrl, ravenDb);
+  }
 
   store.initialize();
   return store;
